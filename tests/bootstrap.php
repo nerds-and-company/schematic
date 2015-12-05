9c70877800ae1f847db0cfd76c6933b99af5fcb3
Bob Olde Hampsink <b.oldehampsink@itmundi.nl>
2015-12-05 19:02:54 +0100

    Dynamic bootstrap paths

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,7 +1,9 @@
 <?php
 
+$rootPath = getenv('IS_TRAVIS') ? __DIR_.'/../' : __DIR__.'/../../../../';
+
 // Require Craft unit test bootstrap
-require_once 'craft/app/tests/bootstrap.php';
+require_once $rootPath.'/craft/app/tests/bootstrap.php';
 
 // Require autoloader
-require_once 'vendor/autoload.php';
+require_once $rootPath.'/vendor/autoload.php';
